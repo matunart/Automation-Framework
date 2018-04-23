7c66378fba9664a87fe746c529deeab2d26e50b0
MOSCOW\Artem_Matyunin <artem.matyunin.consultant@consumer.org>
2018-04-24 00:21:59 +0300

    New features

--- src/main/java/com/qa/basetest/BaseTest.java
+++ src/main/java/com/qa/basetest/BaseTest.java
@@ -1,11 +1,26 @@
 package com.qa.basetest;
 
 
+import com.qa.environment.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class BaseTest {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+    private final static String ENV = System.getProperty("QAENV", "PRODUCTION");
+
+
+    static {
+        try {
+            Environment.readDataFromFile(ENV);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
